8227264493b52df84f9b2297aa883359b9af51ee
maxoness <31696-maxoness@users.noreply.hub.mos.ru>
2025-03-08 00:46:43 +0300

    fixed bug with nested Router

--- frontend/microfrontend/auth/src/components/Register.js
+++ frontend/microfrontend/auth/src/components/Register.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import InfoTooltip from "./InfoTooltip";
 import "../blocks/auth-form/auth-form.css";
 
@@ -42,33 +41,33 @@ function Register({ history }) {
 
   return (
     <>
-      <div className="auth-form">
-        <form className="auth-form__form" onSubmit={handleSubmit}>
-          <div className="auth-form__wrapper">
-            <h3 className="auth-form__title">Регистрация</h3>
-            <label className="auth-form__input">
-              <input type="text" name="email" id="email"
-                className="auth-form__textfield" placeholder="Email"
-                onChange={e => setEmail(e.target.value)} required />
-            </label>
-            <label className="auth-form__input">
-              <input type="password" name="password" id="password"
-                className="auth-form__textfield" placeholder="Пароль"
-                onChange={e => setPassword(e.target.value)} required />
-            </label>
-          </div>
-          <div className="auth-form__wrapper">
-            <button className="auth-form__button" type="submit">Зарегистрироваться</button>
-            <p className="auth-form__text">Уже зарегистрированы? <Link className="auth-form__link" to="/signin">Войти</Link></p>
-          </div>
-        </form>
-      </div>
-      <InfoTooltip
-        onClose={closeAllPopups}
-        onCloseAllPopupsEvent={closeAllPopups}
-        isOpen={isInfoToolTipOpen}
-        status={tooltipStatus}
-      />
+        <div className="auth-form">
+          <form className="auth-form__form" onSubmit={handleSubmit}>
+            <div className="auth-form__wrapper">
+              <h3 className="auth-form__title">Регистрация</h3>
+              <label className="auth-form__input">
+                <input type="text" name="email" id="email"
+                  className="auth-form__textfield" placeholder="Email"
+                  onChange={e => setEmail(e.target.value)} required />
+              </label>
+              <label className="auth-form__input">
+                <input type="password" name="password" id="password"
+                  className="auth-form__textfield" placeholder="Пароль"
+                  onChange={e => setPassword(e.target.value)} required />
+              </label>
+            </div>
+            <div className="auth-form__wrapper">
+              <button className="auth-form__button" type="submit">Зарегистрироваться</button>
+              <p className="auth-form__text">Уже зарегистрированы? <a className="auth-form__link" href="/">Войти</a></p>
+            </div>
+          </form>
+        </div>
+        <InfoTooltip
+          onClose={closeAllPopups}
+          onCloseAllPopupsEvent={closeAllPopups}
+          isOpen={isInfoToolTipOpen}
+          status={tooltipStatus}
+        />
     </>
   )
 }
